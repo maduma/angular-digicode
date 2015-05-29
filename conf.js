43d363626a74ce4bb23cb3f95bfbf7373e413ba4
maduma <maduma@pt.lu>
2015-05-29 23:17:18 +0200

    all test in one conf + cleaning

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 exports.config = {
 
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js'],
+  specs: ['spec.js', 'spec2.js'],
   capabilities: {
     browserName: 'chrome',
     'chromeOptions': {
